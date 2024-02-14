17795ef5b735fb2c60537fc6d75832bdfc90e26a
R-Seel02 <Ryanjseely@icloud.com>
2024-02-14 16:27:12 -0500

    Push 4 adds support for floating point numbers

--- Calculator.java
+++ Calculator.java
@@ -11,21 +11,21 @@ public class Calculator {
         
 
         System.out.println("Welcome to Ryan's Calculator");
-        System.out.println("Please Enter two whole numbers . ");
-       //System.out.println("Please Enter two numbers . ");
-        int x = 0; //push 1 & 2
-        int y = 0;
-        //double y = 0;
-        //double x = 0;
+        //System.out.println("Please Enter two whole numbers . ");
+       System.out.println("Please Enter two numbers . ");
+        //int x = 0; //push 1 & 2
+        //int y = 0;
+        double y = 0;
+        double x = 0;
         //int sum = 0;
         
         
 
         Scanner sc = new Scanner(System.in);
-        x = sc.nextInt();
-        y = sc.nextInt();
-        //y = sc.nextDouble();  //push 4 req
-        //x = sc.nextDouble(); //push 4 req
+        //x = sc.nextInt();
+        //y = sc.nextInt();
+        y = sc.nextDouble();  //push 4 req
+        x = sc.nextDouble(); //push 4 req
 
         //sum = sc.nextInt();
         //System.out.println("would you like to add or subtract"); //push 2 req
@@ -45,59 +45,59 @@ public class Calculator {
       //  System.out.println("Invalid operation selected. Please enter 'add' or 'subtract'.");
     //}
 
-   double result = 0;
-    switch (opp.toLowerCase()) {
-        case "add":
-            result = x + y;
-            System.out.println("The sum of " + x + " and " + y + " is: " + result);
-            break;
-        case "subtract":
-            result = x - y;
-            System.out.println("The difference of " + x + " and " + y + " is: " + result);
-            break;
-        case "multiply":
-            result = x * y;
-            System.out.println("The product of " + x + " and " + y + " is: " + result);                  // required for push 3 
-            break;
-        case "divide":
-            if (y == 0) {
-                System.out.println("Error! Division by zero is not allowed.");   // required for push 5
-            } else {
-                result = (double) x / y;
-                System.out.println("The result of dividing " + x + " by " + y + " is: " + result);
-            }
-            break;
-        default:
-            System.out.println("Invalid operation selected.");
-    }
+//    double result = 0;
+//     switch (opp.toLowerCase()) {
+//         case "add":
+//             result = x + y;
+//             System.out.println("The sum of " + x + " and " + y + " is: " + result);
+//             break;
+//         case "subtract":
+//             result = x - y;
+//             System.out.println("The difference of " + x + " and " + y + " is: " + result);
+//             break;
+//         case "multiply":
+//             result = x * y;
+//             System.out.println("The product of " + x + " and " + y + " is: " + result);                  // required for push 3 
+//             break;
+//         case "divide":
+//             if (y == 0) {
+//                 System.out.println("Error! Division by zero is not allowed.");   // required for push 5
+//             } else {
+//                 result = (double) x / y;
+//                 System.out.println("The result of dividing " + x + " by " + y + " is: " + result);
+//             }
+//             break;
+//         default:
+//             System.out.println("Invalid operation selected.");
+//     }
       
        // sum = x + y ;                  //  required for push 1
 
 
-       // double result = 0;
-       // switch (operation.toLowerCase()) {
-        //    case "add":
-          //      result = x + y;
-         //       System.out.println("The sum of " + x + " and " + y + " is: " + result);
-          //      break;
-         //   case "subtract":
-         //       result = x - y;
-         //       System.out.println("The difference of " + x + " and " + y + " is: " + result);
-          //      break;
-         //   case "multiply":
-         //       result = x * y;
-         //       System.out.println("The product of " + x + " and " + y + " is: " + result);        // push 4 req
-           //     break;
-          //  case "divide":
-           //     if (y == 0) {
-            //        System.out.println("Error! Division by zero is not allowed.");
-           //     } else {
-            //        result = x / y;
-            //        System.out.println("The result of dividing " + x + " by " + y + " is: " + result);
-                //}
-          //      break;
-         //   default:
-         //       System.out.println("Invalid operation selected.");
+        double result = 0;
+        switch (opp.toLowerCase()) {
+            case "add":
+               result = x + y;
+               System.out.println("The sum of " + x + " and " + y + " is: " + result);
+               break;
+           case "subtract":
+               result = x - y;
+               System.out.println("The difference of " + x + " and " + y + " is: " + result);
+               break;
+           case "multiply":
+               result = x * y;
+               System.out.println("The product of " + x + " and " + y + " is: " + result);        // push 4 req
+               break;
+           case "divide":
+               if (y == 0) {
+                   System.out.println("Error! Division by zero is not allowed.");
+               } else {
+                   result = x / y;
+                   System.out.println("The result of dividing " + x + " by " + y + " is: " + result);
+                }
+               break;
+           default:
+               System.out.println("Invalid operation selected.");
 
 
 
@@ -109,14 +109,14 @@ public class Calculator {
         
     //System.out.println("The Sum of the two numbers is " + sum);     //push 1 req 
 
-        sc.close();
+        //sc.close();
         
         
 
     }
-   // sc.close();
+   sc.close();
 
   }
-
+}
     
 
